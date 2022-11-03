04b10c735adc13286005f78572910c16b5ac31f3
Ed Harcourt <ehar@stlawu.edu>
2022-11-02 21:40:34 -0400

    firebase

--- python/firebase_example.py
+++ python/firebase_example.py
@@ -1,6 +1,18 @@
+# https://firebase.google.com/docs/firestore/quickstart
 
 import firebase_admin
 from firebase_admin import credentials
+from firebase_admin import firestore
 
-cred = credentials.Certificate("path/to/serviceAccountKey.json")
+cred = credentials.Certificate("cs345fall22-firebase-adminsdk-3ivaa-67f327fcdb.json")
 firebase_admin.initialize_app(cred)
+
+db = firestore.client()
+
+students = db.collection(u'students')
+docs = students.stream()
+
+for doc in docs:
+    print(f'{doc.id} => {doc.to_dict()}')
+
+print("Done")
